c434913f0997d82a5cc87ac05b7f769eb8a046df
github username <github email address>
2019-02-28 16:55:31 -0600

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+const main = document.getElementById('main')
+const input = document.querySelector('input')
+
+// let divs = document.querySelectorAll('div')
+//
+// const bubble = (e) => {
+//   console.log(this)
+//   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+//
+// }
+// for (let i = 0; i < divs.length; i++) {
+//   divs[i].addEventListener('click', bubble)
+// }
+
+
+input.addEventListener('keydown', (e)=> {
+    if(e.which === 71) {
+      console.log('default prevented')
+      return e.preventDefault()
+    } else {
+      console.log('Not a "g"')
+    }
+  })
+main.addEventListener('click', (event) => {console.log('alert')})
