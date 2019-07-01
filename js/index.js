fc0923d413827d6a2d7cb9912c039c1349a23faa
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-01 17:54:56 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,25 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const main = document.getElementById('main');
+
+console.log("here?");
+main.addEventListener('click', function(event) {
+  console.log('I was clicked!');
+});
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', function(e) {
+  console.log(e.which);
+});
+
+
+divs = document.querySelectorAll('div');
+
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
+}
